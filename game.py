05bee7c6ef6da0017882d6243e88dd869a7e83c0
Andrey <corhal@yandex.ru>
2017-03-10 12:24:04 +0300

    can play from cmd
    "n" to quit, anything else to continue

--- game.py
+++ game.py
@@ -2,20 +2,28 @@ from classes import *
 from data import *
 import copy
 
-mission_results = []
-day_results = []
+def play():
 
-print("-" * 30)
-for i in range(0, 500):    
-    items = load_items()
-    missions = load_missions(items)
-    quests = load_quests(items)
-    player = Player(444, missions)
-    if not player.choose_quest(quests):
+    mission_results = []
+    day_results = []
+
+    print("-" * 30)
+    for i in range(0, 500):    
+        items = load_items()
+        missions = load_missions(items)
+        quests = load_quests(items)
+        player = Player(444, missions)
+        if not player.choose_quest(quests):
+            break
+        mission_results.append(player.missions_completed)
+        day_results.append(player.day)
+
+    if len(mission_results) > 0 and len(day_results) > 0:
+        print("Missions, on average: " + str(sum(mission_results)/float(len(mission_results))))
+        print("Days, on average: " + str(sum(day_results)/float(len(day_results))))
+
+while True:
+    choice = input("Play again? y/n ")
+    if choice == "n":
         break
-    mission_results.append(player.missions_completed)
-    day_results.append(player.day)
-
-if len(mission_results) > 0 and len(day_results) > 0:
-    print("Missions, on average: " + str(sum(mission_results)/float(len(mission_results))))
-    print("Days, on average: " + str(sum(day_results)/float(len(day_results))))
+    play()
